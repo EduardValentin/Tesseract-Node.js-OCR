1c21b7a61de57a15a0780ea21a03590a5273d4b1
Eduard Trocan <eduard.trocan@sparktech.ro>
2018-10-19 05:02:22 -0700

    added sequelize.js to project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
-const express = require('express');
-const database = require('./models/database');
 require('dotenv').config();
+
+const express = require('express');
+const db = require(__dirname + '/models/index');
+
 
 const app = express();
 const port = process.env.PORT;
 
-database.createTables();
-database.dropTables();
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
