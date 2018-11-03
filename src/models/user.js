4e8e9d0efe1129d91bfc272863d7ddb17039e471
Eduard Trocan <eduard.trocan@sparktech.ro>
2018-11-03 13:57:30 +0100

    idk

---
+++ src/models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.NUMBER,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
