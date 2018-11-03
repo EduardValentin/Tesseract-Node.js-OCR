0dc4da5fc6af18e39ebdeeda2534e77d62be0e01
Eduard Trocan <eduard.trocan@sparktech.ro>
2018-11-03 13:01:49 +0100

    config

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ const app = express();
 const port = process.env.PORT;
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.status(200).send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
